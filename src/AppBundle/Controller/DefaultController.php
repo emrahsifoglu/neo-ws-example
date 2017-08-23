b303a0f7110f2d5e6501b56e38aad5994379959f
Emrah Sifoğlu <emrahsifoglu@gmail.com>
2017-08-23 23:00:35 +0300

    Specify a default controller
    for route /
    with a proper json return {"hello":"world!"}

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,17 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+    public function indexAction() {
+        return new JsonResponse([
+            'hello' => 'world'
+        ]);
     }
+
 }
